2ece41855b5726ea6330dac2dec25ecccf30be5d
NSchiffmacher <nathan.schiffmacher@gmail.com>
2024-05-04 12:17:14 +0200

    [flammable] When there's no fire in the area, decrease ignition_probability

--- src/sandsim/behaviors/flammable.rs
+++ src/sandsim/behaviors/flammable.rs
@@ -1,10 +1,10 @@
 use crate::sandsim::behaviors::*;
 
 pub struct Flammable {
-    ignition_rate: f64, // The rate at which "current_ignite_probability" increases for each FIRE_ID in the given radius (Unit: prob/second)
+    ignition_rate: f64, // The rate at which "current_ignition_probability" increases for each FIRE_ID in the given radius (Unit: prob/second)
     ignition_radius: i32, // The radius in which to check for FIRE_ID (Actually, checks on a square of side 2*ignition_radius + 1) 
     
-    current_ignite_probability: f64, // The current probability of igniting
+    current_ignition_probability: f64, // The current probability of igniting
     num_cell_in_radius: f64, // The number of cells in the given radius
 }
 
@@ -15,22 +15,29 @@ impl Behavior for Flammable {
 
     fn update(&mut self, state: &ParticleState, dt: f64, grid: &mut Vec<Vec<ParticleId>>, _behaviors_grid: &mut Vec<Vec<BehaviorId>>) -> Vec<ParticleAction> {
         // Increase ignite probability based on the number of FIRE_ID in the given radius
+        let mut any_fire_in_area = false;
         for i in -self.ignition_radius..=self.ignition_radius {
             for j in -self.ignition_radius..=self.ignition_radius {
                 if i == 0 && j == 0 {
                     continue;
                 }
                 // Check if there is a FIRE_ID in the given radius
-                // If there is, increase the current_ignite_probability
+                // If there is, increase the current_ignition_probability
                 if grid[(state.position.1 + j) as usize][(state.position.0 + i) as usize] == FIRE_ID {
-                    self.current_ignite_probability += self.ignition_rate * dt / self.num_cell_in_radius;
+                    self.current_ignition_probability += self.ignition_rate * dt / self.num_cell_in_radius;
+                    any_fire_in_area = true;
                 }
 
             }
         }
 
+        // If there is no FIRE_ID in the given radius, decrease the current_ignition_probability
+        if !any_fire_in_area && self.current_ignition_probability > 0. {
+            self.current_ignition_probability = 0f64.max(self.current_ignition_probability - self.ignition_rate * dt);
+        }
+
         // Check if the current cell catches fire
-        if rand::random::<f64>() < self.current_ignite_probability {
+        if rand::random::<f64>() < self.current_ignition_probability {
             return vec![
                 ParticleAction::KillParticle { position: state.position },
                 ParticleAction::SpawnParticle { callback: |pos| Particle::new_fire(pos), position: state.position },
@@ -47,7 +54,7 @@ impl Flammable {
             ignition_radius,
             ignition_rate,
 
-            current_ignite_probability: 0.,
+            current_ignition_probability: 0.,
             num_cell_in_radius: ((2 * ignition_radius + 1) * (2 * ignition_radius + 1)) as f64,
         })
     }
